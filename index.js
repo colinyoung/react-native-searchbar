0023fdd9b264aee9402178290bf4f7bcebaa3709
Colin Young <me@colinyoung.com>
2016-12-19 11:31:10 -0600

    Use MaterialIcons from exponent

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   Animated,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcons from '@exponent/vector-icons';
 import { filter, some, includes } from 'lodash/collection';
 import { debounce } from 'lodash/function';
 
